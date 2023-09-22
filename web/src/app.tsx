a3c1acd6b4b780a9803e9716821223826b92e42c
suxing <suxnju@smail.nju.edu.cn>
2023-09-22 17:27:47 +0800

    update json load

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,181 +1,130 @@
-import React, { useState } from 'react'
-import { BASE_URL } from './config/api'
+import { Layout } from 'antd';
+import React, { useEffect, useState } from 'react';
 import {
+    Route,
     BrowserRouter as Router,
     Routes,
-    Route,
-    useMatch,
+    useMatch
 } from "react-router-dom";
-import { SingleDiffContainer } from './containers/diff/SingleDiffContainer';
-import { HomeContent } from './containers/home/Home';
+import { PollingMultiDiffContainer } from './containers/code/PollingMultiDiffContainer';
 import { AppLayout } from './containers/common/AppLayout';
-import { Layout } from 'antd';
-import { PollingMultiDiffContainer } from './containers/diff/PollingMultiDiffContainer';
+import { HomeContent } from './containers/home/Home';
 
-async function initializeStagingDiff(
-    params: StagingParams,
-    setError?: (err: string) => void) {
-    const response = await fetch(
-        `${BASE_URL}/staging_upgrades/${params.id}`,
-        {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            }
-        }
-    )
-    if (response) {
-        const json = await response.json()
-        if (json.status === 'ok') {
-            if (json.data.length) {
-                return json.data[json.data.length - 1]
-            }
-        } else if (json.status === 'nok') {
-            setError(json.msg)
-            return undefined;
-        }
-    }
-    setError('Server has returned an empty response');
-    return undefined;
+export interface BaseParams {
+    network?: string,
+    address?: string,
+    format?: string,
+    language?: string,
 }
+// import sample from "./sample.json"
 
-async function initializeDiff(
-    params: BaseParams,
-    setError?: (err: string) => void
-) {
-    const body = params
 
-    const response = await fetch(
-        `${BASE_URL}/upgrades`,
-        {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                'content-type': 'application/json'
-            }
+const JsonDisplay = () => {
+    const [jsonData, setJsonData] = useState(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          // 根据参数来动态导入不同的 JSON 文件
+          const jsonModule = await import(`../assets/sample.json`);
+          const data = jsonModule.default; // 获取导入的 JSON 数据
+          setJsonData(data);
+        } catch (error) {
+          console.error('Error loading JSON:', error);
         }
-    )
-    if (response) {
-        const json = await response.json()
-        if (json.status === 'ok') {
-            if (json.data.length) {
-                return json.data[json.data.length - 1]
-            }
-        } else if (json.status === 'nok') {
-            setError(json.msg)
-            return undefined;
-        }
+      };
+  
+      fetchData();
+    });
+  
+    if (!jsonData) {
+      return <div>Loading...</div>;
     }
-    setError('Server has returned an empty response');
-    return undefined;
-}
+  
+    return (
+      <div>
+        <h1>JSON Data</h1>
+        <ul>
+          {jsonData.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default JsonDisplay;
 
 async function initializeMultiDiff(
     params: BaseParams
 ) {
+    const [data, setData] = useState<any[]>([]);
     const body = params
+    // const fs = require('fs')
+    // console.log(params)
     const response = await fetch(
-        `${BASE_URL}/upgrades`,
-        {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                'content-type': 'application/json'
-            }
-        }
+        // '../assets/data/' + params.address + "/" + params.language + "/Bytecode.json" 
+        "./sample.json"
     )
-    if (response) {
-        const json = await response.json()
-        if (json.status === 'ok') {
-            //Return the whole array of found upgrades
-            return json.data
-        } else if (json.status === 'nok') {
-            return Promise.reject(json.msg);
-        }
-    }
-    const msg = 'Server has returned an empty response';
+
+    useEffect(() => {
+    // 使用fetch API加载本地JSON文件
+    fetch('./data.json')
+        .then((response) => response.json())
+        .then((jsonData) => setData(jsonData))
+        .catch((error) => console.error('Error loading JSON:', error));
+    }, []);
+
+    console.log(data)
+    // useEffect(() => {
+    //     import("../dist/sample.json")
+    //         .then((response) => response.default)
+    // })
+    // const response = JSON.parse(fs.readFileSync("./dist/sample.json"))
+    // console.log(response)
+    // if (response) {
+    //     const json = await response.json()
+    //     if (json.status === 'ok') {
+    //         //Return the whole array of found upgrades
+    //         return json.data
+    //     } else if (json.status === 'nok') {
+    //         return Promise.reject(json.msg);
+    //     }
+    // }
+    const msg = 'The result of the '+params.address+" is not in the examples";
     return Promise.reject(msg);
 }
 
-export interface BaseParams {
-    address?: string,
-    newImpl?: string,
-    network?: string,
-}
-
-interface StagingParams {
-    id: string
-}
-
-export const DiffRoutes = () => {
+export const CodeRoutes = () => {
     const [error, setError] = useState(undefined);
 
-    const stagingDiffFetch = (params: StagingParams) => {
-        let id = params.id;
-        if ((!id || id === null)) {
-            setError('Id should not be null when querying staging')
+    const baseMatch = useMatch("/reversedcode/:network/:address/:format/:language")
+    const getBaseParams = () => {
+        return {
+            address:baseMatch.params.address,
+            format:baseMatch.params.format,
+            language:baseMatch.params.language
         }
-
-        return initializeStagingDiff(params, setError)
-    }
-
-    const diffFetch = (params: BaseParams) => {
-        return initializeDiff(params, setError)
     }
 
     const multiDiffFetch = (params: BaseParams) => {
         return initializeMultiDiff(params)
     }
 
-    const baseMatch = useMatch("/diffs/:network/:address/:new_impl");
-    const getBaseParams = () => {
-        return {
-            network: baseMatch.params.network,
-            address: baseMatch.params.address,
-            new_impl: baseMatch.params.new_impl
-        };
-    }
-
-    const multiDiffMatch = useMatch("/diffs/:network/:address");
-    const getMultiDiffParams = () => {
-        return {
-            network: multiDiffMatch.params.network,
-            address: multiDiffMatch.params.address
-        };
-    }
-
-    const stagingMatch = useMatch("/staging/:id");
-    const getStagingParams = () => {
-        return {
-            id: stagingMatch.params.id
-        };
-    }
-
     return <Routes>
-        <Route path="/staging/:id"
-            element={
-                <SingleDiffContainer
-                    getSearchParams={getStagingParams}
-                    diffFetchHook={stagingDiffFetch}
-                    setError={setError}
-                />
-            } />
-        <Route path="/diff/:network/:proxy/:new_impl" element={
-            <SingleDiffContainer
-                getSearchParams={getBaseParams}
-                diffFetchHook={diffFetch}
-                setError={setError}
-            />
-        } />
-        <Route path="/diffs/:network/:proxy" element={
+        <Route path="/reversedcode/:network/:address/:format/:language" element={
             <AppLayout>
                 <Layout>
-                    <PollingMultiDiffContainer
-                        getPathParams={getMultiDiffParams}
+                    {/* <div>
+                        <p>Hello ChatGPT</p>
+                    </div> */}
+                    <JsonDisplay />
+                    {/* <PollingMultiDiffContainer
+                        getPathParams={getBaseParams}
                         diffFetchHook={multiDiffFetch}
                         error={error}
                         setError={setError}
-                    />
+                    /> */}
                 </Layout>
             </AppLayout>
         } />
@@ -188,6 +137,6 @@ export const DiffRoutes = () => {
 
 export const App = () => {
     return <Router>
-        <DiffRoutes />
+        <CodeRoutes />
     </Router>
 }
