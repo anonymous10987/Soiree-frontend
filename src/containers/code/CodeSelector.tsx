67310f9d052d6a37e2da52385d848dfb999b9ad6
suxing <suxnju@smail.nju.edu.cn>
2023-09-29 21:09:15 +0800

    update response render

--- src/containers/code/CodeSelector.tsx
+++ src/containers/code/CodeSelector.tsx
@@ -18,32 +18,6 @@ export interface ResponseProps{
 }
 
 export const CodeSelector = (props: ResponseProps) => {
-    console.log(props.response)
-    // return (ReactDom.render(
-    //         <Markdown
-    //         children={props.response}
-    //         components={{
-    //             code(props) {
-    //             const {children, className, inline, node, ...rest} = props
-    //             const match = /output_type-(\w+)/.exec(className || '')
-    //             return !inline && match ? (
-    //                 <SyntaxHighlighter
-    //                 {...rest}
-    //                 children={String(children).replace(/\n$/, '')}
-    //                 style={vs}
-    //                 showLineNumbers
-    //                 output_type={match[1]}
-    //                 PreTag="div"
-    //                 />
-    //             ) : (
-    //                 <code {...rest} className={className}>
-    //                 {children}
-    //                 </code>
-    //             )
-    //             }
-    //         }}
-    //         ></Markdown>
-    // ))
     return (
         <div>
           <ReactMarkdown
